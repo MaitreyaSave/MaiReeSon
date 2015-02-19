b784d4f90ffe0a67ee49e47621054eafa36ab2ee
MaitreyaSave <maitreya.save@gmail.com>
2015-02-19 16:08:49 +0530

    add

---
+++ Untitled.m
@@ -0,0 +1,52 @@
+%converting to gray scale
+in = imread('F:\image.png');
+bw=rgb2gray(in);
+
+%noise removal using median filter
+img=medfilt2(bw,[5 5]);
+bw=double(img);
+[row, col] = size(bw);
+for i= 1:row
+    for j=1:col
+        if i>=79 && i<=84
+            bw(i,j)=255;
+        end
+        if i>=189 && i<=191
+            bw(i,j)=255;
+        end
+    end
+end
+B=im2bw(bw);
+B=~B;
+simg=imfill(B,'holes');
+
+[bw_labelled num]=bwlabel(simg);
+
+j=0;
+for i=1:length(areas)
+    areas= regionprops(bw_labelled, 'BoundingBox');
+    subImage = imcrop(simg, areas(i).BoundingBox);
+    [x,y]=size(subImage);
+    disp(size(subImage));
+    if((x*y)>50)
+        j=j+1;
+        imwrite(subImage,strcat('char',num2str(j),'.png'));
+    end
+end
+
+
+%areas1=[];
+%for i=1:length(areas);
+ %   areas1=[areas1; areas(i,1).Area];
+%end
+%disp(areas1);
+
+%index=find(areas1>100 & areas1<2000);
+%finalimg= zeros(size(bw_labelled));
+%for i=1:length(index)
+ %   image=bw_labelled==index(i);
+  %  imwrite(image,strcat('char',num2str(i),'.png'));
+   % disp(strcat('char',num2str(i),'.png'));
+%end
+
+
